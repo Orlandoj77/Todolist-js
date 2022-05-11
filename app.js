cd89ea643a4395948a70c3576a7e269041aabc31
Orlandoj77 <laudesjones@gmail.com>
2022-05-11 19:42:06 +0100

    todo-list/ criando o função que se se encarrega de criar o itens

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let banco = [];
+
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
+
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item);
+}
